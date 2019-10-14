6bcedb185236a6157534980e88269b4251bd297f
Rubi Kina <rkina@at-l1.21-school.ru>
2019-10-14 15:20:57 +0300

    add gnl

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkina <rkina@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/05 19:45:00 by rkina             #+#    #+#             */
+/*   Updated: 2019/10/10 15:57:31 by rkina            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "./libft/libft.h"
+
+# define BUFF_SIZE 1
+
+int		get_next_line(const int fd, char **line);
+
+#endif
